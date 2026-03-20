ea6773883dbab75ca59e33f703e68d0d81667d69
Tejeswini <324103310208@gvpce.ac.in>
2026-03-20 14:54:16 +0530

    Add method to find minimum absolute difference in submatrix
    Implement minAbsDiff method to calculate minimum absolute difference in sliding submatrices of size k.

---
+++ Matrix/LC_3567: Minimum absolute difference in sliding submatrix.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def minAbsDiff(self, grid, k):
+        rows = len(grid)
+        cols = len(grid[0])
+
+        res = [[0] * (cols - k + 1) for u in range(rows - k + 1)]
+
+        for i in range(rows - k + 1):
+            for j in range(cols - k + 1):
+                arr = []
+                for r in range(i, i + k):
+                    for c in range(j, j + k):
+                        arr.append(grid[r][c])
+                if k == 1:
+                    res[i][j] = 0
+                    continue
+                arr = sorted(set(arr))
+                if len(arr) <= 1:
+                    res[i][j] = 0
+                    continue
+                mini = float('inf')
+                for p in range(1,len(arr)):
+                    mini = min(mini, arr[p] - arr[p-1])
+                
+                res[i][j] = mini
+
+        return res
